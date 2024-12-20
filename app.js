3e65aafa29ab61495d93f15b171f80c945ac93ae
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2024-12-20 16:05:34 +0200

    add health endpoint

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
 
-app.get('/version', (req, res) => {
+app.get('/version', (_req, res) => {
   res.send('1');
+});
+
+app.get('/health', (_req, res) => {
+  res.send('ok');
 });
 
 app.listen(PORT, () => {
